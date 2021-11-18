16e8bcf3649bd8ca1cf7a573046b56ee89d86787
LABO13-12-USER <LABO13-12-USER@satis.local>
2021-11-18 21:10:46 +0100

    1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Les films à voir avant de mourir</title>
+		<meta charset="utf-8">
+        <a href="info.php">Pour plus d'infos, cliquez ici </a>
+        
+	</head>
+    
+	<body>
+    </body>
+</html>
